8f10cbc6d1e6a1f135ee82a679f452ba59c9cfc6
jztang <12689662+jztang@users.noreply.github.com>
2023-03-31 10:39:00 -0400

    Create operate function

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "x":
+      return multiply(num1, num2);
+    case "÷":
+      return divide(num1, num2);
+  }
+}
+
+let num1;
+let num2;
+let operator;
